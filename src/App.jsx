b7c9f00d422e664e7fea32d220fe820dd5b62e18
Victor Vasquez <victrex1234@gmail.com>
2024-02-28 09:38:43 -0600

    enrutado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import './assets/css/index.css'
+import { HashRouter, Route, Routes } from "react-router-dom";
+import "./assets/css/index.css";
 
 function App() {
-
   return (
-    <>
-      <h1>PROYECTO INGENIERIA DE SOFTWARE</h1>
-    </>
-  )
+    <HashRouter>
+      <div className="app">
+        <Routes>
+          <Route path="/register" element={'s'}></Route>
+          <Route path="/login" element={'s'}></Route>
+          <Route path="/terms" element={'s'}></Route>
+        </Routes>
+      </div>
+    </HashRouter>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const queryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")).render(
+  /* Antes estaba el <> en <React.StrictMode></React.StrictMode> 
+esto hace que se renderice dos la aplicacion */
+  <>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </>
+);
