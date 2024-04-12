58e1ae587bd4f7283281913aa86cb81b4a502d42
Victor Vasquez <victrex1234@gmail.com>
2024-04-11 20:02:40 -0600

    trabajando en subida de fotos por csv

--- src/components/products/ProductPhotosFromCSV.jsx
+++ src/components/products/ProductPhotosFromCSV.jsx
@@ -1,104 +1,72 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from "react";
 import "../../assets/css/productPhotos.css";
 import AddIcon from "@mui/icons-material/Add";
 
-const ProductPhotosFromCSV = ({ idProduct }) => {
-  const [photos, setPhotos] = useState([]);
-  const [video, setVideo] = useState(null);
-  const [data, setData] = useState({ idProduct, photos: [], video: null});
-  const [id, setId] = useState(idProduct);
-  
-  // const { photos, setPhotos, video, setVideo } = useContext(Context);
+const ProductPhotosFromCSV = ({ idProduct, useMedia}) => {
+  // const [photos, setPhotos] = useState([]);
+  // const [video, setVideo] = useState(null);
+  // const [data, setData] = useState({ idProduct, photos: [], video: null});
+  // const [id, setId] = useState(idProduct);
+
+  // const sub = (e) => {
+  //   console.log(id)
+  //   const file = Array.from(e.target.files);
+  //   if (!file) return;
+
+  //   file?.forEach((file) => {
+  //     // console.log(file);
+  //     if (file.type == "video/mp4") {
+  //       // console.log("es video");
+  //       setVideo({ file, name: fileNameCure(file.name) });
+  //     } else {
+  //       if(idProduct === data.idProduct){
+  //         setData({ ...data, photos: [...data.photos, { file, name: fileNameCure(file.name) }] });
+  //       }
+  //       // setData({ ...data, photos: [...data.photos, { file, name: fileNameCure(file.name) }] });
+  //       // console.log("no es video");
+  //       setPhotos((currentPhotos) => {
+  //         if (currentPhotos.length < 6) {
+  //           console.log(file);
+  //           return [...currentPhotos, { file, name: fileNameCure(file.name) }];
+  //         } else {
+  //           alert("Solo es permitido 6 fotos y un video");
+  //           return currentPhotos;
+  //         }
+  //       });
+  //     }
+  //   });
+  //   e.preventDefault();
+  // };
+  const { photos, videos, handleSetPhotos, handleSetVideos, handleClickDeletePhoto, handleClickDeleteVideo, } = useMedia()
 
   const sub = (e) => {
-    console.log(id)
     const file = Array.from(e.target.files);
     if (!file) return;
 
     file?.forEach((file) => {
-      // console.log(file);
       if (file.type == "video/mp4") {
-        // console.log("es video");
-        setVideo({ file, name: fileNameCure(file.name) });
+        handleSetVideos({ idProduct, file, name: fileNameCure(file.name) })
       } else {
-        if(idProduct === data.idProduct){
-          setData({ ...data, photos: [...data.photos, { file, name: fileNameCure(file.name) }] });
-        }
-        // setData({ ...data, photos: [...data.photos, { file, name: fileNameCure(file.name) }] });
-        // console.log("no es video");
-        setPhotos((currentPhotos) => {
-          if (currentPhotos.length < 6) {
-            console.log(file);
-            return [...currentPhotos, { file, name: fileNameCure(file.name) }];
-          } else {
-            alert("Solo es permitido 6 fotos y un video");
-            return currentPhotos;
-          }
-        });
+        handleSetPhotos({ idProduct, file, name: fileNameCure(file.name) })
       }
     });
     e.preventDefault();
   };
 
-
   const getFile = () => {
-    document.getElementById("media").click();
+    document.getElementById("media"+idProduct).click();
   };
 
   const fileNameCure = (name) => {
-    let special = [
-      "/",
-      "\\",
-      "|",
-      " ",
-      " +",
-      "-",
-      "@",
-      "#",
-      "^",
-      "-",
-      ";",
-      ":",
-      "`",
-      "'",
-      '"',
-      "*",
-    ];
+    let special = [ "/", "\\", "|", " ", " +", "-", "@", "#", "^", "-", ";", ":", "`", "'", '"', "*", ];
     special.forEach((i) => {
       name = name.replaceAll(i, "");
     });
     return Math.floor(Math.random() * Date.now() * 10000).toString(36) + name;
   };
 
-  const isImageOrVideo = (blob) => {
-    const type = blob.type;
+  
 
-    if (type.startsWith("image/")) {
-      return "image";
-    } else if (type.startsWith("video/")) {
-      return "video";
-    } else {
-      return "unknown";
-    }
-  };
-
-  const handleClickDeletePhoto = (name) => {
-    const blob = new Blob([name], { type: "image/png" });
-    const type = isImageOrVideo(blob);
-    console.log(type, name);
-
-    setPhotos(
-      photos.filter((e) => {
-        const condition = typeof name === "string" ? name : name.name;
-        if (typeof e === "string") {
-          return e != condition;
-        } else if (typeof e === "object") {
-          return e.name != condition;
-        }
-      })
-    );
-  };
   const renderImage = (photo) => {
     if (typeof photo === "string") {
       // Si la foto es una URL, usarla directamente
@@ -109,9 +77,9 @@ const ProductPhotosFromCSV = ({ idProduct }) => {
     }
   };
 
-  useEffect(() => {
-    console.log(data)
-  }, [data]);
+  // useEffect(() => {
+  //   console.log(data)
+  // }, [data]);
 
   return (
     <>
@@ -119,42 +87,50 @@ const ProductPhotosFromCSV = ({ idProduct }) => {
         <small> Imágenes | La primera imagen será la principal</small>
       </h4>
       <div className="mediaContainer">
-        {photos?.map((photo, i) => (
-          <div key={"photo" + i} className="picturePreview">
-            <button
-              className="btn-delete-media"
-              type="button"
-              onClick={() => handleClickDeletePhoto(photo)}
-            >
-              x
-            </button>
-            <img src={renderImage(photo)} alt="Profile" />
-          </div>
-        ))}
+        {photos && photos?.map((photo, i) => {
+          if (photo.idProduct == idProduct) 
+          { return (
+            <div key={photo.name + i} className="picturePreview">
+              <button
+                className="btn-delete-media"
+                type="button"
+                onClick={() => handleClickDeletePhoto(photo)}
+              >
+                x
+              </button>
+              <img src={renderImage(photo)} alt="Profile" />
+            </div>)
+          } else 
+        return null
+        })}
 
-        {video ? (
-          <div className="picturePreview">
-            <button
-              className="btn-delete-media"
-              type="button"
-              onClick={() => setVideo(null)}
-            >
-              x
-            </button>
-            <video
-              width="640"
-              height="360"
-              src={
-                video?.file instanceof Blob
-                  ? URL.createObjectURL(video.file)
-                  : video
-              }
-              autoPlay
-              muted
-              loop
-            />
-          </div>
-        ) : null}
+        {videos && videos?.map((video, i) =>  {
+          if (video.idProduct == idProduct) {
+            return (
+            <div key={video.name + i} className="picturePreview">
+              <button
+                className="btn-delete-media"
+                type="button"
+                onClick={() => handleClickDeleteVideo(video.name)}
+              >
+                x
+              </button>
+              <video
+                width="640"
+                height="360"
+                src={
+                  video?.file instanceof Blob
+                    ? URL.createObjectURL(video.file)
+                    : video
+                }
+                autoPlay
+                muted
+                loop
+              />
+            </div>)
+          } else 
+          return null
+        })}
         {/* {video && (
           <div className="picturePreview">
             <button
@@ -181,11 +157,10 @@ const ProductPhotosFromCSV = ({ idProduct }) => {
 
       <input
         type="file"
-        id="media"
+        id={"media" + idProduct}
         onChange={sub}
         accept=".jpg, .jpeg, .png, .mp4"
         style={{ height: "0px", width: "0px", overflow: "hidden" }}
-        multiple
       />
 {/*       <input
         type="file"
--- src/components/products/UploadSrcToProductsByCSV.jsx
+++ src/components/products/UploadSrcToProductsByCSV.jsx
@@ -1,9 +1,70 @@
 // Desc: Upload a CSV file to update products
+import { useState, useEffect } from "react";
 
 import { Button } from "../generalComponents/Button";
 import ProductPhotosFromCSV from "./ProductPhotosFromCSV";
 
 const UploadSrcToProductsByCSV = () => {
+
+  const [photos, setPhotos] = useState([]);
+  const [videos, setVideos] = useState([]);
+
+  const useMedia = () => {
+
+  const handleSetPhotos = (photo) => {
+    if (photos.filter( e => e.idProduct == photo.idProduct).length >= 6)
+    return alert("Solo se pueden subir un 6 fotos por producto")
+    setPhotos( [...photos, {...photo}]  )
+  }
+  
+  const handleSetVideos = (video) => {
+    if (videos.some( (e) => e.idProduct == video.idProduct ))
+    return alert("Solo se puede subir un video por producto")
+    setVideos( [...videos, {...video}]  )
+  }
+
+  const handleClickDeletePhoto = (name) => {
+    const blob = new Blob([name], { type: "image/png" });
+    const type = isImageOrVideo(blob);
+
+    setPhotos(
+      photos.filter((e) => {
+        const condition = typeof name === "string" ? name : name.name;
+        if (typeof e === "string") {
+          return e != condition;
+        } else if (typeof e === "object") {
+          return e.name != condition;
+        }
+      })
+    );
+  };
+
+  const handleClickDeleteVideo = (name) => {
+    setVideos( videos.filter((e) => e.name != name))
+  }
+
+  const isImageOrVideo = (blob) => {
+    const type = blob.type;
+
+    if (type.startsWith("image/")) {
+      return "image";
+    } else if (type.startsWith("video/")) {
+      return "video";
+    } else {
+      return "unknown";
+    }
+  };
+
+    return {
+      photos,
+      videos,
+      handleSetPhotos,
+      handleSetVideos,
+      handleClickDeletePhoto,
+      handleClickDeleteVideo,
+    }
+  }
+
   return (
     <div className="contentByCSV">
       <section className="productsByCSVContainer uploadPhotos">
@@ -11,11 +72,11 @@ const UploadSrcToProductsByCSV = () => {
         <section className="productItemsContent">
           <div className="itemUploadingContainer">
             <h3 className="titleItemUploading">Wine - Sogrape Mateus Rose</h3>
-            <ProductPhotosFromCSV idProduct={"01"} />
-          </div>
+            <ProductPhotosFromCSV idProduct={"01"} useMedia={useMedia}/>
+           </div>
           <div className="itemUploadingContainer">
-            <h3 className="titleItemUploading">Wine - Sogrape Mateus Rose</h3>
-            <ProductPhotosFromCSV idProduct={"2"} />
+            <h3 className="titleItemUploading">Wine - Del super</h3>
+            <ProductPhotosFromCSV idProduct={"2"} useMedia={useMedia}/>
           </div>
           
         </section>
